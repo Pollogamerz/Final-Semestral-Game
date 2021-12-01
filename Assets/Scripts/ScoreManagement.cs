e7aa5653911c936771c484ec96d2b104f7adc098
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2021-11-30 22:39:24 -0600

    update

--- Assets/Scripts/ScoreManagement.cs
+++ Assets/Scripts/ScoreManagement.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class ScoreManagement : MonoBehaviour
 {
     public Text scoreText;
+    public Text highScore;
     private float score;
 
 
-
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +21,22 @@ public class ScoreManagement : MonoBehaviour
         {
             score += 1 * Time.deltaTime;
             scoreText.text = ((int)score).ToString();
+
+            if (score > PlayerPrefs.GetInt("HighScore", 0))
+            {
+                PlayerPrefs.SetInt("HighScore", (int)score);
+                highScore.text = score.ToString();
+
+            }
         }
+
+
     }
+
+    /*public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+        highScore.text = "0";
+    }*/
+
 }
